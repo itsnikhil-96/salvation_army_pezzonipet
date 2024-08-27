6023dcd1a90492351d73d9ba2e2a2372a4915a1f
NIKHIL <nikhilpolimetla852@gmail.com>
2024-08-27 20:39:46 +0530

    commit1

--- frontend/src/RootLayout.jsx
+++ frontend/src/RootLayout.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header3 from "./components/header1/Header1";
-import Mainheading from './components/mainheading/Mainheading';
 import Footer from "./components/footer/Footer";
 import { Outlet } from "react-router-dom";
 import './Rootlayout.css';
