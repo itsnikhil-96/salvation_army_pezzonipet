056115199d50ea4ee62c530576e133e909f52fcd
NIKHIL <nikhilpolimetla852@gmail.com>
2024-08-31 18:10:47 +0530

    ewww

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
   build: {
-      outDir: 'build',
-  }
+    outDir: 'dist',  // Ensure this is set to 'dist'
+  },
 });
