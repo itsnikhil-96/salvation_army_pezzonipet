06f0fd0f80465a0a4c22ba2f0e5205e94b8e5269
NIKHIL <nikhilpolimetla852@gmail.com>
2024-08-31 18:06:22 +0530

    letshopebest

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      external: [
-        'browser-image-compression',
-      ],
-    },
-  },
+      outDir: 'build',
+  }
 });
