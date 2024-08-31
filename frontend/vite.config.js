2d159ba50cf2148a2c6f18d12c501117377fdf7d
NIKHIL <nikhilpolimetla852@gmail.com>
2024-08-31 18:13:02 +0530

    mallierror

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   build: {
-    outDir: 'dist',  // Ensure this is set to 'dist'
+    outDir: 'dist',
   },
 });
